156456db9db84178e2a98c3d98251ab887bade8f
薛凯 <3440395@qq.com>
2017-05-11 18:32:37 +0800

    提交修改

---
+++ app/src/main/java/com/rxjava2/android/samples/ui/myrxbus/RxBus.java
@@ -0,0 +1,48 @@
+package com.rxjava2.android.samples.ui.myrxbus;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
+
+/**
+ * Created by xuekai on 2017/5/11.
+ */
+
+public class RxBus {
+    private static volatile RxBus defaultInstance;
+
+    private final Subject<Object> bus;
+
+    // PublishSubject只会把在订阅发生的时间点之后来自原始Observable的数据发射给观察者
+    public RxBus() {
+        bus = PublishSubject.create();
+    }
+
+    public static RxBus getDefault() {
+        if (defaultInstance == null) {
+            synchronized (RxBus.class) {
+                if (defaultInstance == null) {
+                    defaultInstance = new RxBus();
+                }
+            }
+        }
+        return defaultInstance;
+    }
+
+    // 发送一个新的事件
+    public void post(Object o) {
+        bus.onNext(o);
+    }
+
+    // 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者
+    public <T> Observable<T> toObservable(Class<T> eventType) {
+        return bus.ofType(eventType);
+//        这里感谢小鄧子的提醒: ofType = filter + cast
+//        return bus.filter(new Func1<Object, Boolean>() {
+//            @Override
+//            public Boolean call(Object o) {
+//                return eventType.isInstance(o);
+//            }
+//        }) .cast(eventType);
+    }
+}
